b9c8cbc7bacec651a4bf80cd0f09bea5f75fd98e
FNC-pinoFraddosio <benedetto.fraddosio@marconibari.edu.it>
2024-11-27 18:13:46 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -6,20 +6,48 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        int n, imin;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.print("Inserire la grandezza del vettore: ");
+        n = in.nextInt();
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        String[] nomi = new String[n];
+        String[] nazionali = new String[n];
+        float[] tempi = new float[n];
+
+        for(int i = 0; i < n; i++){
+            System.out.print("Nome " + (i+1) + "° atleta: ");
+            nomi[i] = in.next();
+            System.out.print("Nazionale "  + (i+1) + "° atleta: ");
+            nazionali[i] = in.next();
+            System.out.print("Tempo " + (i+1) + "° atleta: ");
+            tempi[i] = in.nextFloat();
+        }
+
+        System.out.println("Informazioni inserite: ");
+
+        for(int i = 0; i < n; i++){
+            System.out.println((i+1) + "° atlteta: " + nomi[i] + "; nazionale: " + nazionali[i] + "; tempo: " + tempi[i]);
+        }
+
+        System.out.println("Ricerca dell'atleta vincitore");
+        float min;
+
+        imin = 0;
+        min = tempi[0];
+
+        for(int i = 0; i < nomi.length; i++){
+            if(tempi[i] < min){
+                imin = i;
+                min = tempi[i];
+            }
+        }
+
+        System.out.println("Atleta vincitore: " + nomi[imin] + "; nazionale: " + nazionali[imin] + "; tempo: " + tempi[imin]);
+
     }
 }
 
